043f3689ac1c98a9565500adbd56585ba10bb7b3
dzachesov <dzachesov@odin.com>
2018-09-19 09:30:03 +0300

    fix firefox bug

--- src/components/CurrencyInput/index.js
+++ src/components/CurrencyInput/index.js
@@ -7,7 +7,15 @@ import style from './style.css'
 
 const InputNumber = ({onValueChange}) => (
     <input autoFocus={true} className={style.inputNumber} 
-            type="number"
+            type="text" // Used 'text' instead of "number", because Firefox has a bug with input charachters. And if entered value is NaN then e.target.value is empty.
+            onKeyPress = {e => {
+                if(
+                    !/[0-9\.]/.test(e.key) ||
+                    (['.'].includes(e.key) && (e.target.value.includes('.')))
+                ){
+                    e.preventDefault();
+                }
+            }}
             onChange={(e) => onValueChange(e)}/>
 )
 
@@ -16,8 +24,14 @@ export const CurrencyInput = compose(
     observer,
     mapProps(({Converter}) => ({
         onValueChange: e => {
+
             if(e.target.value.length > 6){
                 e.target.value = e.target.value.slice(0,6);
+            }
+
+            if(isNaN(Number(e.target.value))){
+                e.preventDefault();
+                return; 
             }
 
             Converter.setFromValue(Number(e.target.value));
